2c2891033529ce555b1d244b5b65552e92cf1be8
Andres David <acala244@unab.edu.co>
2026-09-18 09:38:11 -0500

    Contador funcional

--- app/src/main/java/me/andrescala/statecompose/StateScreen.kt
+++ app/src/main/java/me/andrescala/statecompose/StateScreen.kt
@@ -1,5 +1,6 @@
 package me.andrescala.statecompose
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,9 +9,14 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -18,16 +24,22 @@ import androidx.compose.ui.unit.sp
 @Preview (showSystemUi = true, showBackground = true)
 @Composable
 fun StateScreen(){
+    var contador by remember { mutableStateOf(3) }
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxSize()
     ) {
-        Text(text = "Contador: 0",
+        Text(text = "Contador: $contador",
             fontSize = 32.sp)
         Spacer(modifier = Modifier.height(15.dp))
-        Button(onClick = {}) {
-            Text(text = "Este es un boton")
+        Button(onClick = {
+            Log.i("mis errores", "Estoy haciendo click!")
+            contador = contador + 1
+        }) {
+            Text(text = "Sumar",
+                fontWeight = FontWeight.Bold
+                )
         }
     }
 }
